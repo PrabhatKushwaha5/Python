1ca08c9f203094c0f07419abb3854d2eab569101
Prabhat Kushwaha <147148541+PrabhatKushwaha5@users.noreply.github.com>
2025-03-06 23:22:52 +0530

    Add files via upload

---
+++ excapeSequance.py
@@ -0,0 +1,9 @@
+# escape sequance is used to give new line to a prticular code 
+print("hello \n world")
+
+#if we want to give cotation  inside a string we use double cot in ouside and single cot inside 
+print("flat is '4bhk' in this society")
+
+#another method of using the cotation inside the string is 
+
+print("my name is \"Prabhat\" and i am a engineer")
